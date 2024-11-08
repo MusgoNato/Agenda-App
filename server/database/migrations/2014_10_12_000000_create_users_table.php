127c53d49e79c2a5fcf18298885f97db1b794ec7
CauanDev <keldev2004@gmail.com>
2024-11-08 19:19:52 -0400

    feat: master user

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB; // Necessário para a inserção de dados
 
 return new class extends Migration
 {
@@ -16,12 +17,22 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('status');
+            $table->string('status')->nullable();
             $table->rememberToken();
-            $table->timestamps(); 
+            $table->timestamps();
             $table->timestamp('deleted_at')->nullable();
             $table->timestamp('authenticated_at')->nullable();
         });
+
+        // Insere o usuário "master" logo após a criação da tabela
+        DB::table('users')->insert([
+            'name' => 'Master User',
+            'email' => 'master@example.com',
+            'password' => bcrypt('master123'), // Criptografa a senha "master123"
+            'status' => 'active', // Definindo status como ativo, por exemplo
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
